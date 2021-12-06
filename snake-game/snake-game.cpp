8da4ed35f9892e246ace3e92169ca9d95dfe8ab5
UIT-20520336 <90953797+UIT-20520336@users.noreply.github.com>
2021-12-07 04:30:30 +0700

    fix minor (#3)

--- snake-game/snake-game.cpp
+++ snake-game/snake-game.cpp
@@ -4,53 +4,70 @@
 #include <conio.h>
 using namespace std;
 void gotoxy(int column, int line);
+int Width_Box = 40, Height_Box = 40;
 struct Point {
     int x, y;
 };
-class CONRAN {
+class SNAKE {
 public:
     struct Point A[100];
-    int DoDai;
+    int Snake_Length;
     SNAKE() {
-        DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
-        A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        Snake_Length = 3;
+        A[0].x = Width_Box / 2 + 2; A[0].y = Height_Box / 2;
+        A[1].x = Width_Box / 2 + 1; A[1].y = Height_Box / 2;
+        A[2].x = Width_Box / 2; A[2].y = Height_Box / 2;
     }
-    void Ve() {
-        for (int i = 0; i < DoDai; i++) {
+    void Draw_Snake() {
+        for (int i = 0; i < Snake_Length; i++) {
             gotoxy(A[i].x, A[i].y);
             cout << "X";
         }
     }
-    void DiChuyen(int Huong) {
-        for (int i = DoDai - 1; i > 0; i--)
+    void move(Direction direction) {
+        for (int i = Snake_Length - 1; i > 0; i--)
             A[i] = A[i - 1];
-        if (Huong == 0) A[0].x = A[0].x + 1;
-        if (Huong == 1) A[0].y = A[0].y + 1;
-        if (Huong == 2) A[0].x = A[0].x - 1;
-        if (Huong == 3) A[0].y = A[0].y - 1;
+        if (direction == Direction::right) A[0].x = A[0].x + 1;
+        if (direction == Direction::down) A[0].y = A[0].y + 1;
+        if (direction == Direction::left) A[0].x = A[0].x - 1;
+        if (direction == Direction::up) A[0].y = A[0].y - 1;
 
     }
+    // kiểm tra rắn chạm tường
+    bool isHitwall()
+    {
+        return A[0].x == 0 || A[0].y == 0 || A[0].x == Width_Box || A[0].y == Height_Box;
+    }
+    //kiểm tra rắn tự cắn chính mình
+    bool isBiteItself()
+    {
+        Point head = A[0];
+        for (size_t i = 1; i < Snake_Length; i++)
+            if (head.x == A[i].x && head.y == A[i].y)
+                return true;
+        return false;
+    }
 };
-
+SNAKE r;
 int main()
 {
-    CONRAN r;
-    int Huong = 0;
+    Direction direction = Direction::right;
     char t;
-
     while (1) {
         if (kbhit()) {
             t = getch();
-            if (t == 'a') Huong = 2;
-            if (t == 'w') Huong = 3;
-            if (t == 'd') Huong = 0;
-            if (t == 'x') Huong = 1;
+            if (t == 'a' && direction != Direction::right)
+                direction = Direction::left;
+            else if (t == 's' && direction != Direction::down)
+                direction = Direction::up;
+            else if (t == 'w' && direction != Direction::up)
+                direction = Direction::down;
+            else if (t == 'd' && direction != Direction::left)
+                direction = Direction::right;
         }
         system("cls");
-        r.Ve();
-        r.DiChuyen(Huong);
+        r.Draw_Snake();
+        r.move(direction);
         Sleep(300);
     }
 
@@ -92,19 +109,7 @@ void draw_Box(int Height_Box, int Width_Box)
     }
 }
 
-//vị trí rắn xuất hiện: giữa khung
-void startSnake()
-{
-    direction = Direction::right;
-    SNAKE = 
-    {
-        Point{ Width_Box / 2 + 2, Height_Box / 2 },
-        Point{ Width_Box / 2 + 1, Height_Box / 2 },
-        Point{ Width_Box / 2, Height_Box / 2 },
-        Point{ Width_Box / 2 - 1, Height_Box / 2 },
-        Point{ Width_Box / 2 - 2, Height_Box / 2 }
-    };
-}
+
 enum class Direction
 {
     up,
@@ -113,33 +118,7 @@ enum class Direction
     left
 };
 
-//hàm tránh quay đầu 180 độ
-void Checking_180(char direct)
-{
-    if (direct == 'a' && direction != Direction::right)
-        direction = Direction::left;
-    else if (direct == 'x' && direction != Direction::down)
-        direction = Direction::up;
-    else if (direct == 'w' && direction != Direction::up)
-        direction = Direction::down;
-    else if (direct == 'd' && direction != Direction::left)
-        direction = Direction::right;
-    else if (direct == 'q') 
-        break;
-}
 
-// kiểm tra rắn chạm tường
-bool isHitwall()
-{
-    return SNAKE[0].x == 0 || SNAKE[0].y == 0 || SNAKE[0].x == Width_Box || SNAKE[0].y == Height_Box;
-}
 
-//kiểm tra rắn tự cắn chính mình
-bool isBiteItself()
-{
-    Point head = SNAKE[0];
-    for (size_t i = 1; i < SNAKE.size(); i++)
-        if (head.x == SNAKE[i].x && head.y == SNAKE[i].y)
-            return true;
-    return false;
-}
+
+
